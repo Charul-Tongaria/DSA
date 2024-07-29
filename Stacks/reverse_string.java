64ae7e2c54f5f001e10bbf72d712878c10eb16bb
Charul-Tongaria <charultongaria.1703@poornima.org>
2024-07-29 20:33:19 +0530

    DSA

---
+++ Stacks/reverse_string.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class reverse_string {
+    public static String reverse(String str){
+        Stack<Character> s = new Stack<>();
+        int idx = 0;
+        while(idx <= str.length()){
+            s.push(str.charAt(idx));
+            idx++;
+        }
+        StringBuilder result = new StringBuilder("");
+        while(!s.isEmpty()){
+            char curr = s.pop();
+            result.append(curr);
+        }
+        return result.toString();
+    }
+    public static void main(String args[]){
+        String str = "abc";
+        System.out.println(reverse(str));
+    }
+}
